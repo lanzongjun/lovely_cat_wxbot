ce11cc62947c2913262048bdd74dad416ff5a84b
Zongjun Lan <lanzj@elinkint.com>
2020-08-24 17:12:51 +0800

    modify config url

--- config.php
+++ config.php
@@ -4,6 +4,6 @@ $config = array( 'allowed_ip' => '127.0.0.1', /*白名单ip（运行微信客户
 	'group_main' => array( '19429208528@chatroom', '271286322@chatroom', '2819762314@chatroom', '20197283301@chatroom'), /*所有主群*/
 	'group_admin' => array( 'admin' => '1345456778@chatroom' ), /*所有管理群*/
 	'admin' => array( 'mxyr' => 'lanzongjun', 'czy' => 'wxid_ci4ski7wxyzw22' ), /*所有管理员*/
-	'url' => "http://192.168.2.69:8073/send", /*框架调用地址*/
+	'url' => "http://127.0.0.1:8073/send", /*框架调用地址*/
 	'custom_key' => '344523435', /*自定义密钥*/
 );
